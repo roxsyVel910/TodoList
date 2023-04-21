35c3450a89f03bd27d26857d7c35cd42f6961b99
Roxsy <roxsy.dj@gmail.com>
2023-04-21 13:34:11 -0500

    agrando funcionalidad a todoIten y TodoCouter

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,11 +1,20 @@
-import React, { Component } from 'react'
+import React from 'react';
+import './TodoItem.css';
 
-export default class TodoItem extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
+function TodoItem(props) {
+  return (
+    <li className="TodoItem">
+      <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}>
+        √
+      </span>
+      <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
+        {props.text}
+      </p>
+      <span className="Icon Icon-delete">
+        X
+      </span>
+    </li>
+  );
 }
+
+export { TodoItem };
