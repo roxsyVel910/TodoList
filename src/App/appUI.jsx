b2fbaf6659956a3646cfa0cebb97a305c1849d54
Roxsy <roxsy.dj@gmail.com>
2023-04-25 08:15:55 -0500

    estructura de archivos en app

---
+++ src/App/appUI.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { TodoItem } from '../TodoItem';
+import { TodoList } from '../TodoList';
+import { TodoCounter } from '../TodoCounter';
+import { TodoSearch } from '../TodoSearch';
+import { CreateTodoButton } from '../CreateTodoButton';
+
+
+
+function AppUI({
+    totalTodos,
+     completedTodos,
+    searchValue,
+      setSearchValue,
+
+    searchedTodos,
+        completeTodo,
+       deleteTodo
+
+}){
+  return (
+    <React.Fragment>
+       <TodoCounter
+       total = { totalTodos}
+       completed = { completedTodos}
+        />
+      <TodoSearch 
+      searchValue={searchValue}
+      setSearchValue={setSearchValue}
+      />
+      <TodoList>
+        {searchedTodos.map(todo => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+            onComplete = {() => completeTodo(todo.text)}
+            onDelete = {() => deleteTodo(todo.text)}
+          />
+        ))}
+      </TodoList>
+
+      <CreateTodoButton />
+    </React.Fragment>
+  );
+}
+
+export {AppUI};
---
+++ src/App/index.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import {AppUI} from "./appUI";
+
+const defaultTodos = [
+    { text: 'Cortar cebolla', completed: true },
+    { text: 'Tomar el cursso de intro a React', completed: false },
+    { text: 'Llorar con la llorona', completed: false },
+    { text: 'LALALALAA', completed: false },
+  ];
+  
+  function App() {
+    
+    const [ todos, setTodos] = React.useState(defaultTodos);
+  
+    const [searchValue, setSearchValue] = React.useState('')
+    // Cantidad de TODOs completados
+    const completedTodos = todos.filter(todo => !!todo.completed).length;
+    // Cantidad total de TODOs
+    const totalTodos = todos.length;
+  
+    let searchedTodos = [];
+  
+    if(!searchValue.length >= 1){
+      searchedTodos = todos;
+    } else {
+      searchedTodos = todos.filter(todo => {
+        const todoText = todo.text.toLowerCase();
+        const searchText = searchValue.toLowerCase();
+        return todoText.includes(searchText)
+  
+      });
+    }
+  
+    const completeTodo = (text) => {
+      const todoIndex = todos.findIndex(todo => todo.text === text);
+      const newTodos = [... todos];
+      todos[todoIndex].completed = true;
+      setTodos(newTodos);
+  
+    }
+    const deleteTodo = (text) => {
+      const todoIndex = todos.findIndex(todo => todo.text === text);
+      const newTodos = [... todos];
+      newTodos.splice(todoIndex, 1);
+      setTodos(newTodos);
+  
+    }
+   
+  
+  
+    return (
+      <AppUI
+      totalTodos={totalTodos}
+      completedTodos={completedTodos}
+      searchValue={searchValue}
+      setSearchValue={setSearchValue}
+      searchedTodos={searchedTodos}
+      completeTodo={completeTodo}
+      deleteTodo={deleteTodo}
+
+      />
+    );
+  }
+  
+  export default App;
