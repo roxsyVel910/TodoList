f4d10aef7aeb0d68bc98ccb103ccc40d295004b0
Roxsy <roxsy.dj@gmail.com>
2023-04-25 00:10:53 -0500

    manejo de estado

--- src/components/TodoSearch.jsx
+++ src/components/TodoSearch.jsx
@@ -1,14 +1,21 @@
+import React from 'react';
+import '../stylos/TodoSearch.css';
+
 function TodoSearch() {
-    const onSearchValueChange = (event) => {
-      console.log(event.target.value);
-    };
-    
-    return (
-      <input
-        className="TodoSearch"
-        placeholder="Cebolla"
-        onChange={onSearchValueChange}
-      />
-    );
-  }
-  export { TodoSearch };
+    const [patito, setPatito] = React.useState('Juan');
+
+  /*const onSearchValueChange = (event) => {
+    console.log(event.target.value);
+  };*/
+  
+  return [
+    <input
+      className="TodoSearch"
+      placeholder="Cebolla"
+      onChange={() => setPatito('Enrique')}
+    />,
+    <p>{patito}</p>
+  ];
+}
+
+export { TodoSearch };
